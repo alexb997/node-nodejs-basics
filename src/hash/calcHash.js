0bb7a61376b3ce0661f0ec04a814bb584dc54983
alexb997 <alex_alexandru_997@yahoo.ro>
2025-04-27 13:12:38 +0300

    Stream hash and cli commits

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  const hash = createHash("sha256");
+  const stream = createReadStream(filePath);
+
+  stream.on("data", (chunk) => hash.update(chunk));
+  stream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
+  stream.on("error", () => {
+    throw new Error("FS operation failed");
+  });
 };
 
 await calculateHash();
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const filePath = join(__dirname, "files", "fileToRead.txt");
+  const readableStream = createReadStream(filePath);
+
+  readableStream.pipe(process.stdout);
 };
 
 await read();
